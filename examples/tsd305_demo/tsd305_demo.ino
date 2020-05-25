39e18b3a69177815d8fcb46ad56e43916de1eb53
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-05-25 16:53:40 +0530

    example added

---
+++ examples/tsd305_demo/tsd305_demo.ino
@@ -0,0 +1,40 @@
+/*
+* Description :   Example of TSD305 Sensor (by TE-Connectivity) for Arduino platform.
+* Author      :   Pranjal Joshi
+* Date        :   25/05/2020 
+* License     :   MIT
+* This code is published as open source software. Feel free to share/modify.
+*/
+
+#include <tsd305lib.h>
+
+tsd305 tsd;
+tsd_eeprom_struct tes;
+
+float sensorTemp, objDeg, objFar;
+
+void setup() {
+  bool debug = false;                   // Change to true to read sensor parameters while calling other functions       
+  Serial.begin(115200);
+  Serial.println();
+  tes = tsd.begin(debug);               // Prints debuging values over serial while accessing TSD305
+}
+
+void loop() {
+  if(tsd.isConnected()) {
+    sensorTemp = tsd.getSensorTemp();
+    objDeg = tsd.getObjectTemp();
+    objFar = tsd.DtoF(objDeg);
+    Serial.print("Sensor (Ambient) Temperature (°C): ");
+    Serial.println(sensorTemp);
+    Serial.print("Object Temperature (°C): ");
+    Serial.println(objDeg);
+    Serial.print("Object Temperature (°F): ");
+    Serial.println(objFar);
+    Serial.println("\n");
+  }
+  else {
+    Serial.println("TSD305 not connected.");
+  }
+  delay(1000);
+}
