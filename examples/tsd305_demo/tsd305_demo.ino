98417a03c49724171f84dd571b6e6532010851b0
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-05-29 10:30:14 +0530

    v1.0.3 - comments added for better readability

--- examples/tsd305_demo/tsd305_demo.ino
+++ examples/tsd305_demo/tsd305_demo.ino
@@ -14,17 +14,26 @@ tsd_eeprom_struct tes;
 float sensorTemp, objDeg, objFar;
 
 void setup() {
-  bool debug = false;                   // Change to true to read sensor parameters while calling other functions       
-  Serial.begin(115200);
+  bool debug = false;                    // Change to true to read sensor parameters while calling other functions       
+  Serial.begin(115200);                 // Change baudrate according to your application
   Serial.println();
   tes = tsd.begin(debug);               // Prints debuging values over serial while accessing TSD305
+  /*
+   If sensor debugging is not required, you can use following as well
+   tes = tsd.begin();
+   If sensor's measurement limits are not required, use following
+   tsd.begin();
+   */
 }
 
 void loop() {
-  if(tsd.isConnected()) {
-    sensorTemp = tsd.getSensorTemp();
-    objDeg = tsd.getObjectTemp();
-    objFar = tsd.DtoF(objDeg);
+  if(tsd.isConnected()) {               // Check if sensor is connected over I2C bus
+    
+    sensorTemp = tsd.getSensorTemp();   // Get temperature of sensor itself i.e. ambient temperature
+    objDeg = tsd.getObjectTemp();       // Get temperature of object
+    objFar = tsd.DtoF(objDeg);          // Convert value from °C to °F
+
+    // Print measured data on serial monitor
     Serial.print("Sensor (Ambient) Temperature (°C): ");
     Serial.println(sensorTemp);
     Serial.print("Object Temperature (°C): ");
